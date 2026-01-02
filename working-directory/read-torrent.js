86ac62bbdb3154c8df775490a40a22f1ea05856e
Sahil Mane <148791198+sahilmane69@users.noreply.github.com>
2026-01-02 16:20:49 +0530

    Add tracker and torrent parsing logic

--- working-directory/read-torrent.js
+++ working-directory/read-torrent.js
@@ -7,9 +7,46 @@
 // const torrent = fs.readFileSync("sample.torrent");
 // console.log(torrent.toString());
 
-const fs = require("fs");
+// const fs = require("fs");
+//
+// const torrent = fs.readFileSync("sample.torrent");
+// // console.log("Torrent file loaded. Size:", torrent.length, "bytes");
+// console.log({
+//     announce: torrent.announce,
+//     name: torrent.info.name,
+//     pieceLength: torrent.info['piece length'],
+//     totalPieces: torrent.info.pieces.length / 20
+// });
 
-const torrent = fs.readFileSync("sample.torrent");
-console.log("Torrent file loaded. Size:", torrent.length, "bytes");
+
+
+//------------------------------------------------------------------------------------------------
+
+import fs from "fs";
+import bencode from "bencode";
+import crypto from "crypto";
+
+const torrentFile = fs.readFileSync("./sample.torrent");
+const torrent = bencode.decode(torrentFile);
+
+// Correct access using Buffers
+const info = torrent[Buffer.from("info")];
+
+const name = Buffer.from(info[Buffer.from("name")]).toString("utf8");
+
+const pieceLength = info[Buffer.from("piece length")];
+const pieces = info[Buffer.from("pieces")];
+
+console.log({
+    name,
+    pieceLength,
+    totalPieces: pieces.length / 20
+});
+
+// Generate info hash
+const infoEncoded = bencode.encode(info);
+const infoHash = crypto.createHash("sha1").update(infoEncoded).digest("hex");
+
+console.log("Info Hash:", infoHash);
 
 
